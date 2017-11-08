79237de86a314801f76f7462a06fcf21c9b027e7
NigulasiZhao <185653517@qq.com>
2017-11-08 23:15:38 +0800

    新建项目

---
+++ Lucien-EasyUIDemo/Lucien-EasyUIDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lucien_EasyUIDemo.MvcApplication" Language="C#" %>
---
+++ Lucien-EasyUIDemo/Lucien-EasyUIDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lucien_EasyUIDemo.Startup))]
+namespace Lucien_EasyUIDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
